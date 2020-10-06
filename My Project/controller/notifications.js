4f804d5ec3f68c932108442df711a3ff43e5abc8
Vladimir Gekov <vladimirgekov7@gmail.com>
2020-10-06 13:57:04 +0300

    Add files via upload

---
+++ My Project/controller/notifications.js
@@ -0,0 +1,8 @@
+export function notify(message, selector) {
+  const notification = document.querySelector(selector);
+  notification.textContent = message;
+  notification.style.display = "block";
+  notification.addEventListener("click", () => {
+    notification.style.display = "none";
+  });
+}
